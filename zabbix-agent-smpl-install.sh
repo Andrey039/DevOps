c015f172e90dbac1a755be73d902867c5590bdeb
Andrey Maslov <maslov.kng@gmail.com>
2021-02-16 15:07:26 +0200

    Update zabbix-agent-smpl-install.sh

--- zabbix-agent-smpl-install.sh
+++ zabbix-agent-smpl-install.sh
@@ -1,8 +1,14 @@
-#!/bin/bash
+!/bin/bash
 wget https://repo.zabbix.com/zabbix/5.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_5.0-1+focal_all.deb
 dpkg -i zabbix-release_5.0-1+focal_all.deb && rm zabbix-release_5.0-1+focal_all.deb
 apt update
 apt install zabbix-agent -y
 
+sed -i -E "s/^Hostname=.*$/Hostname=$HOSTNAME/g" /etc/zabbix/zabbix_agentd.conf
+sed -i -E "s/^Server=.*$/Server=$1/g" /etc/zabbix/zabbix_agentd.conf
+
 systemctl enable zabbix-agent
 systemctl restart zabbix-agent
+
+status=$(systemctl status  zabbix-agent | grep Active)
+echo $status
