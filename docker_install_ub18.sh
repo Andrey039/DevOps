cb0369933540bec6b25086287ee45413cd7a3e51
Andrey Maslov <maslov.kng@gmail.com>
2021-10-13 19:37:20 +0200

    Update docker_install_ub18.sh

--- docker_install_ub18.sh
+++ docker_install_ub18.sh
@@ -16,6 +16,6 @@ apt-get -y install docker-ce
 systemctl enable docker && systemctl start docker
 
 # Install docker-compose
-curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" \
+curl -L "https://github.com/docker/compose/releases/download/3.8/docker-compose-$(uname -s)-$(uname -m)" \
 -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
